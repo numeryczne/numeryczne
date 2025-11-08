19265dfc037e5c76e9c2645caadcc1c2054002d3
numeryczne <gustawkonrad@int.pl>
2025-11-08 14:27:06 +0000

    yy

--- sk.ps1
+++ sk.ps1
@@ -1,8 +1,8 @@
-$fileUrl = "https://github.com/numeryczne/numeryczne/raw/main/plik.zip"
-$destinationPath = "$env:USERPROFILE\plik.zip"
+$fileUrl = "https://github.com/numeryczne/numeryczne/raw/main/publish.zip"
+$destinationPath = "$env:USERPROFILE\publish.zip"
 Invoke-WebRequest -Uri $fileUrl -OutFile $destinationPath
 
 cd "$env:USERPROFILE"
-Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('.\plik.zip', '.');
-$exePath = ".\Debug\aplikacja1.exe"
+Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('.\publish.zip', '.');
+$exePath = ".\publish\aplikacja1.exe"
 Start-Process -FilePath $exePath
