092135a9010c78326bf9b25afb99405246d5155e
numeryczne <gustawkonrad@int.pl>
2025-11-08 14:36:35 +0000

    yy2

--- sk.ps1
+++ sk.ps1
@@ -1,6 +1,6 @@
 $fileUrl = "https://github.com/numeryczne/numeryczne/raw/main/publish.zip"
 $destinationPath = "$env:USERPROFILE\publish.zip"
-Invoke-WebRequest -Uri $fileUrl -OutFile $destinationPath
+curl -L $fileUrl -o $destinationPath
 
 cd "$env:USERPROFILE"
 Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('.\publish.zip', '.');
